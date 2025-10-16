8e7f07f159ace64a47b7597422946d50db0982d3
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-16 22:07:05 +0300

    Снова поднимаемся вверх
    Затем создайте функцию handlePlay внутри компонента Game, которая будет вызываться компонентом Board для обновления игры. Передайте xIsNext, currentSquares и handlePlay в качестве свойств компоненту Board
    
    https://react.dev/learn/tutorial-tic-tac-toe#lifting-state-up-again

--- react.dev/src/App.js
+++ react.dev/src/App.js
@@ -63,9 +63,16 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
+
   return (
     <div className="game">
       <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
         <Board />
       </div>
       <div className="game-info">
