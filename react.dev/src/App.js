8335e80c15fabefe15554902bcc14417f964d089
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-16 22:08:31 +0300

    Снова поднимаемся вверх
    Давайте сделаем так, чтобы компонент Board полностью управлялся получаемыми им свойствами. Измените компонент Board так, чтобы он принимал три свойства: xIsNext, squares, а также новую функцию onPlay, которую Board может вызывать с обновлённым массивом квадратов, когда игрок делает ход. Затем удалите первые две строки функции Board, которые вызывают useState
    
    https://react.dev/learn/tutorial-tic-tac-toe#lifting-state-up-again

--- react.dev/src/App.js
+++ react.dev/src/App.js
@@ -4,7 +4,7 @@ function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
